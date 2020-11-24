11e089201d0dbd45031d3d1e46dbece36eb0f1ac
msallent <mqsallent@gmail.com>
2020-11-24 01:04:40 -0300

    Add data for navbar links

---
+++ src/data/navbarLinks.ts
@@ -0,0 +1,11 @@
+interface NavbarLink {
+  title: string;
+  href: string;
+}
+
+export const navbarLinks: Array<NavbarLink> = [
+  { title: 'Work', href: '/work' },
+  { title: 'Albums', href: '/albums' },
+  { title: 'About', href: '/about' },
+  { title: 'Contact', href: '/contact' },
+];
