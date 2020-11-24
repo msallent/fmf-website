1b3d96053531323bbe1ae929f6f610eb116f71bc
msallent <mqsallent@gmail.com>
2020-11-24 01:06:04 -0300

    Add Header and Footer to Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,44 @@
 import React, { FunctionComponent } from 'react';
-import { PageProps } from 'gatsby';
+import { graphql, PageProps, useStaticQuery } from 'gatsby';
 import { SEO } from '../SEO';
+import { Header } from '../Header';
+import { Footer } from '../Footer';
+import '../../style/main.scss';
+import 'normalize.css';
 
 export type LayoutProps = Omit<PageProps, 'children'>;
 
-export const Layout: FunctionComponent<LayoutProps> = ({ children, location }) => (
-  <>
-    <SEO location={location} />
-    {children}
-  </>
-);
+interface LayoutData {
+  site: {
+    siteMetadata: {
+      title: string;
+    };
+  };
+}
+
+export const Layout: FunctionComponent<LayoutProps> = ({ children, location }) => {
+  const {
+    site: {
+      siteMetadata: { title },
+    },
+  } = useStaticQuery<LayoutData>(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <>
+      <SEO location={location} />
+      <Header title={title} currentLocation={location} />
+      {children}
+      <Footer title={title} />
+    </>
+  );
+};
