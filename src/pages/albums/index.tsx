c5070c774ad4eda36f163fe24167cdd53d41a91c
msallent <mqsallent@gmail.com>
2020-11-24 01:22:48 -0300

    Add placeholder page for Albums section

---
+++ src/pages/albums/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+import { PageProps } from 'gatsby';
+import { PageTitle } from '../../components/PageTitle';
+
+const Albums: FunctionComponent<PageProps> = () => (
+  <main>
+    <PageTitle title="Albums" />
+  </main>
+);
+
+export default Albums;
