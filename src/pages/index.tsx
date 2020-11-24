82473551506a44ee7f5c5f8a43a880f3ac08653b
msallent <mqsallent@gmail.com>
2020-11-24 01:07:30 -0300

    Add content to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import React, { FunctionComponent } from 'react';
 import { PageProps } from 'gatsby';
-import { ReactComponent as Logo } from '../assets/svg/gatsby.svg';
+import { PageTitle } from '../components/PageTitle';
 import styles from '../style/pages/index.module.scss';
 
 const Index: FunctionComponent<PageProps> = () => (
-  <div className={styles.wrapper}>
-    <Logo className={styles.logo} />
-    <p>Delete this file to start your project.</p>
-    <a className={styles.link} href="https://github.com/msallent/gatsby-starter-skeleton">
-      Gatsby Skeleton
-    </a>
-  </div>
+  <main>
+    <PageTitle title="Creative Audio Producer" />
+    <div className="container">
+      <p className={styles.paragraph}>I make original sound and music for commercial use.</p>
+      <img src="https://via.placeholder.com/1000x450" alt="Placeholder" width="1000" height="450" />
+    </div>
+  </main>
 );
 
 export default Index;
