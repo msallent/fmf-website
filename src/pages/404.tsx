eb3a148517b9a9f5aa53e7c9e99f67053162eddd
Matias Sallent <matias.sallent@mediamonks.com>
2020-11-30 17:11:03 -0300

    Add content and styles to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,18 @@
 import React, { FunctionComponent } from 'react';
-import { PageProps } from 'gatsby';
+import { Link, PageProps } from 'gatsby';
+import classNames from 'classnames';
+import { PageTitle } from '../components/PageTitle';
+import styles from '../style/pages/404.module.scss';
 
-const NotFound: FunctionComponent<PageProps> = () => <div>Error 404 - Not Found</div>;
+const NotFound: FunctionComponent<PageProps> = () => (
+  <main>
+    <PageTitle title="404 - Not Found" />
+    <div className={classNames('container', styles.notFound)}>
+      <h1>Error 404</h1>
+      <h2>Not Found</h2>
+      <Link to="/albums/world-music-album">Go back</Link>
+    </div>
+  </main>
+);
 
 export default NotFound;
