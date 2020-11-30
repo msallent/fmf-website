7c3117f25e0f1c9d7840d5bc84ae2869d0b2b89f
Matias Sallent <matias.sallent@mediamonks.com>
2020-11-30 16:58:28 -0300

    Handle redirect with gatsby-node

--- src/gatsby/gatsby-node.ts
+++ src/gatsby/gatsby-node.ts
@@ -4,4 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+import { GatsbyNode } from 'gatsby';
+
+export const createPages: GatsbyNode['createPages'] = ({ actions: { createRedirect } }) => {
+  createRedirect({
+    fromPath: '/',
+    toPath: '/albums/world-music-album',
+    redirectInBrowser: true,
+    isPermanent: true,
+  });
+};
