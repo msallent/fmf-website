26437c0eebe3844ce3dfe5f360082804edc30f17
Matias Sallent <matias.sallent@mediamonks.com>
2020-11-30 01:03:37 -0300

    Add animations on page change

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,12 +1,34 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
 import styles from './PageTitle.module.scss';
 
 interface PageTitleProps {
   title: string;
 }
 
-export const PageTitle: FunctionComponent<PageTitleProps> = ({ title }) => (
-  <div className={styles.pageTitle}>
-    <div className={styles.titleWrapper}>{title}</div>
-  </div>
-);
+export const PageTitle: FunctionComponent<PageTitleProps> = ({ title }) => {
+  const titleRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      titleRef.current,
+      {
+        y: 30,
+        autoAlpha: 0,
+      },
+      {
+        y: 0,
+        autoAlpha: 1,
+        duration: 0.35,
+      }
+    );
+  }, []);
+
+  return (
+    <div className={styles.pageTitle}>
+      <div className={styles.titleWrapper} ref={titleRef}>
+        {title}
+      </div>
+    </div>
+  );
+};
