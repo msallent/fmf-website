134fbfbdbbe8dd3d1a7c32b58fb668e70622494f
Matias Sallent <matias.sallent@mediamonks.com>
2020-12-09 15:27:07 -0300

    Add data for World Music Album

---
+++ src/data/albums.ts
@@ -0,0 +1,65 @@
+interface AlbumTrack {
+  title: string;
+  artwork: string;
+  description: string;
+  audioFile: string;
+}
+
+export const worldMusicAlbum: Array<AlbumTrack> = [
+  {
+    title: 'Lost & Sand',
+    artwork: '/images/albums/world-music-album/lost-&-sand.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/lost-&-sand.mp3',
+  },
+  {
+    title: 'Dynasty',
+    artwork: '/images/albums/world-music-album/dynasty.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/dynasty.mp3',
+  },
+  {
+    title: 'Pause',
+    artwork: '/images/albums/world-music-album/pause.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/pause.mp3',
+  },
+  {
+    title: 'Rum',
+    artwork: '/images/albums/world-music-album/rum.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/rum.mp3',
+  },
+  {
+    title: 'Monomyth',
+    artwork: '/images/albums/world-music-album/monomyth.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/monomyth.mp3',
+  },
+  {
+    title: 'Voodoo',
+    artwork: '/images/albums/world-music-album/voodoo.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/voodoo.mp3',
+  },
+  {
+    title: 'American Soil',
+    artwork: '/images/albums/world-music-album/american-soil.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/american-soil.mp3',
+  },
+  {
+    title: 'Exotica',
+    artwork: '/images/albums/world-music-album/exotica.jpg',
+    description:
+      'World music, also called universal music or global music is a contemporary musical genre created in order to integrate in a broad concept all traditional or flok music, popular music, ethnic music and other local or characeristic genres of some areas or cultures of the world in particular that tend to be difficult to categorize for the general public. The phenomenon of universal music is closely related to that of globalization and cultural diversity',
+    audioFile: '/audio/albums/world-music-album/exotica.mp3',
+  },
+];
